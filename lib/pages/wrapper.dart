053c0745a7dc9d0d5e5f1a5e117aa1a261877820
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-10-25 22:54:09 +0700

    cap nhat lai wrapper

--- lib/pages/wrapper.dart
+++ lib/pages/wrapper.dart
@@ -1,5 +1,5 @@
-import 'package:monney_management/view/chose_datetime.dart';
-import 'package:monney_management/widgets/splash_page.dart';
+import 'package:monney_management/pages/home/home_page.dart';
+import 'login/login.dart';
 import 'package:provider/provider.dart';
 import 'package:monney_management/models/user.dart';
 import 'package:flutter/material.dart';
@@ -11,10 +11,10 @@ class Wrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     final user=Provider.of<MyUser?>(context);
     if(user!=null){
-      return const TextScreen();
+      return const HomePage();
     }
     else {
-      return const Myscreen() ;
+      return const Login();
     }
   }
 }
