985e7c8eb7440e62a73c5b690b45c32dc6c16ee2
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-10-25 22:55:28 +0700

    them cac chuc nang dang nhap va dang xuat

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,43 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:monney_management/models/user.dart';
+import 'package:monney_management/services/database.dart';
 
+
+class AuthService{
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  MyUser? userFromFirebase(User? user){
+    return user!=null? MyUser(uid:user.uid):null;
+  }
+  Stream<MyUser?> get user{
+    return auth.authStateChanges().map(userFromFirebase);
+  }
+  Future signUpEmail(String email,String password)async{
+    try{
+      UserCredential result= await auth.createUserWithEmailAndPassword(email: email, password: password);
+      User? user=result.user;
+      await Database(uid:user!.uid).updateData('your name','your fullname',"gender");
+      return userFromFirebase(user);
+    }
+    catch(e){
+      print(e);
+    }
+  }
+  Future signOut() async{
+    try{
+      return auth.signOut();
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+  Future signInemailandpassword(String email,String password) async{
+    try{
+      UserCredential result= await auth.signInWithEmailAndPassword(email: email, password: password);
+      User? user= result.user;
+      return userFromFirebase(user);
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+}
