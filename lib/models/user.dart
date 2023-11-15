8ef5ddf2d15f587a9426e4385eed736c17e0041c
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-11-15 12:59:05 +0700

    update uid and asset in class UserInformation

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,6 @@
-class UserInfo{
-  String? username,fullname,gender;
-  UserInfo({this.gender,this.username,this.fullname});
+class UserInformation{
+  String? username,fullname,asset,uid;
+  UserInformation({this.username,this.fullname,this.asset,this.uid});
 }
 
 class MyUser{
