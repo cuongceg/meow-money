68868f328fa0e423ca24264c8ab712d80af0ac82
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-11-19 23:56:39 +0700

    add new streams

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:monney_management/models/money.dart';
 import 'package:monney_management/models/user.dart';
 import 'package:monney_management/services/auth_service.dart';
 import 'package:monney_management/pages/splash_page.dart';
@@ -27,7 +28,14 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         StreamProvider<MyUser?>.value(value: AuthService().user, initialData:null),
-        StreamProvider<List<UserInformation>?>.value(value:Database().authData, initialData: null)
+        StreamProvider<List<UserInformation>?>.value(value:Database().authData, initialData: null),
+        StreamProvider<List<BillsCosmetic>?>.value(value: Database().cosmeticBillsData, initialData:null),
+        StreamProvider<List<BillsClothes>?>.value(value: Database().clothesBillsData, initialData:null),
+        StreamProvider<List<BillsFood>?>.value(value: Database().foodBillsData, initialData:null),
+        StreamProvider<List<BillsPet>?>.value(value: Database().petBillsData, initialData:null),
+        StreamProvider<List<BillsTravel>?>.value(value: Database().travelBillsData, initialData:null),
+        StreamProvider<List<BillsVehicles>?>.value(value: Database().vehiclesBillsData, initialData:null),
+        StreamProvider<List<Goals>?>.value(value: Database(uid:"izbFkTMPYiSeWrHlatRdwzXblf42").goalsCosmeticData, initialData:null),
       ],
       child:const MaterialApp(
           debugShowCheckedModeBanner: false,
