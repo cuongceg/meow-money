d3652edd26154995f70266e6547aef56245a4b1a
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-11-19 23:55:17 +0700

    get data

--- lib/pages/home/bill_history_screen/bill_history.dart
+++ lib/pages/home/bill_history_screen/bill_history.dart
@@ -1,8 +1,10 @@
  import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:monney_management/models/money.dart';
+import 'package:monney_management/models/user.dart';
 import 'package:monney_management/const_value.dart';
 import 'package:monney_management/pages/home/bill_history_screen/bill_charts.dart';
-import 'package:quickalert/quickalert.dart';
+import 'package:provider/provider.dart';
 
 class BillHistory extends StatefulWidget {
   const BillHistory({super.key});
@@ -20,6 +22,89 @@ class _BillHistoryState extends State<BillHistory> {
   Widget build(BuildContext context) {
     double widthR=MediaQuery.of(context).size.width;
     double heightR=MediaQuery.of(context).size.height;
+    final user = Provider.of<MyUser>(context);
+    final billClothesList=Provider.of<List<BillsClothes>?>(context);
+    final billCosmeticList=Provider.of<List<BillsCosmetic>?>(context);
+    final billFood=Provider.of<List<BillsFood>?>(context);
+    final billPet=Provider.of<List<BillsPet>?>(context);
+    final billTravel=Provider.of<List<BillsTravel>?>(context);
+    final billVehicles=Provider.of<List<BillsVehicles>?>(context);
+    List product=[];
+    double expenses=0,cloExpenses=0,cosExpenses=0,foodExpenses=0,petExpenses=0,travelExpenses=0,vehExpenses=0;//sum of money in 1 month
+    if(billClothesList!=null){
+      for(int i=0;i<billClothesList.length;i++){
+        if(billClothesList[i].uid==user.uid){
+          expenses+=double.parse(billClothesList[i].money);
+          cloExpenses+=double.parse(billClothesList[i].money);
+          product.add(billClothesList[i]);
+        }
+      }
+    }
+    if(billCosmeticList!=null){
+      for(int i=0;i<billCosmeticList.length;i++){
+        if(billCosmeticList[i].uid==user.uid){
+          expenses+=double.parse(billCosmeticList[i].money);
+          cosExpenses+=double.parse(billCosmeticList[i].money);
+          product.add(billCosmeticList[i]);
+        }
+      }
+    }
+    if(billFood!=null){
+      for(int i=0;i<billFood.length;i++){
+        if(billFood[i].uid==user.uid){
+          expenses+=double.parse(billFood[i].money);
+          foodExpenses+=double.parse(billFood[i].money);
+          product.add(billFood[i]);
+        }
+      }
+    }
+    if(billPet!=null){
+      for(int i=0;i<billPet.length;i++){
+        if(billPet[i].uid==user.uid){
+          expenses+=double.parse(billPet[i].money);
+          petExpenses+=double.parse(billPet[i].money);
+          product.add(billPet[i]);
+        }
+      }
+    }
+    if(billTravel!=null){
+      for(int i=0;i<billTravel.length;i++){
+        if(billTravel[i].uid==user.uid){
+          expenses+=double.parse(billTravel[i].money);
+          travelExpenses+=double.parse(billTravel[i].money);
+          product.add(billTravel[i]);
+        }
+      }
+    }
+    if(billVehicles!=null){
+      for(int i=0;i<billVehicles.length;i++){
+        if(billVehicles[i].uid==user.uid){
+          expenses+=double.parse(billVehicles[i].money);
+          vehExpenses+=double.parse(billVehicles[i].money);
+          product.add(billVehicles[i]);
+        }
+      }
+    }
+    cloExpenses/=expenses;
+    cosExpenses/=expenses;
+    foodExpenses/=expenses;
+    petExpenses/=expenses;
+    travelExpenses/=expenses;
+    vehExpenses/=expenses;
+    Map<String,double>map={
+      "Clothes":cloExpenses,
+      "Cosmetic":cosExpenses,
+      "Food":foodExpenses,
+      "Pet":petExpenses,
+      "Travel":travelExpenses,
+      "Vehicles":vehExpenses
+    };
+    var mapEntries = map.entries.toList()
+      ..sort((a, b) => a.value.compareTo(b.value));
+    map..clear()..addEntries(mapEntries);
+    List options=map.keys.toList();
+    List percent=map.values.toList();
+    expenses/=1000;
     return AnnotatedRegion(
       value:SystemUiOverlayStyle(
         statusBarColor:Colors.orange.shade100,
@@ -107,7 +192,7 @@ class _BillHistoryState extends State<BillHistory> {
                             child: Text("Expenses",style: Font().headingBlack,),
                           ),
                           Padding(
-                            padding: const EdgeInsets.only(left:10,right:100,top:15),
+                            padding: const EdgeInsets.only(left:10,right:80,top:15),
                             child: Text("Incomes",style: Font().headingBlack,),
                           ),
                         ],
@@ -116,7 +201,7 @@ class _BillHistoryState extends State<BillHistory> {
                         children: [
                           Padding(
                             padding: const EdgeInsets.only(left:10,right:100,top:15,bottom:20),
-                            child: Text("-1.690.000",style: Font().bodyBlack,),
+                            child: Text('-${expenses.toStringAsFixed(3)}.000',style: Font().bodyBlack,),
                           ),
                           Padding(
                             padding: const EdgeInsets.only(left:10,right:15,top:15,bottom:20),
@@ -148,7 +233,8 @@ class _BillHistoryState extends State<BillHistory> {
                     topRight:Radius.circular(40)
                 )
             ),
-            child:const BillChart(),
+            child:BillChart(first:percent[5]*100,second:percent[4]*100,third:percent[3]*100,fourth:(1-percent[5]-percent[4]-percent[3])*100
+            ,sfirst:options[5],ssecond: options[4],sthird: options[3]),
           ),
         ),
       ),
